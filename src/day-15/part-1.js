a555063d56a3fecdaba97e672e177a0bd462ae46
River Marks <rmarks6767@gmail.com>
2021-12-17 22:52:13 -0500

    Finished day 15 and added day 16

--- src/day-15/part-1.js
+++ src/day-15/part-1.js
@@ -1,38 +1,57 @@
 import { getInput } from '../helpers/index.js';
 
 const solution = async () => {
-  const input = await getInput('15', input => input.trim().split('\n').map(i => i.trim().split('').map(j => parseInt(j))));
-  const queue = [{ 
-    x: 0, 
-    y: 0, 
-    risk: input[0][0],
-    prev: null,
-  }];
-  const end = [{ 
-    x: input.length[0] - 1, 
-    y: input.length - 1, 
-    risk: input[input.length - 1][input.length[0] - 1],
-    prev: null,
-  }];
+  let input = await getInput('15', input => input.trim().split('\n').map(i => i.trim().split('').map(j => parseInt(j))));
+  
+  input = input.map((row, y) => {
+    return row.map((risk, x) => ({
+      x,
+      y,
+      risk,
+      r: Number.MAX_VALUE,
+      prev: null,
+      visited: false,
+    }));
+  });
+  
+  const calculatePath = endNode => {
+    let totalCost = 0;
+    let current = endNode;
+    while(current.prev) {
+      totalCost += current.risk;
+      current = input[current.prev[0]][current.prev[1]]
+    }
+
+    return totalCost;
+  }
+
+  const queue = [input[0][0]];
+  queue[0].r = 0;
+  const end = input[input.length - 1][input[0].length - 1];
 
   while(queue.length !== 0) {
-    const curr = queue.sort((c1, c2) => c1.risk - c2.risk).pop();
-    if(curr.x === end.x && curr.y === end.y) return curr.risk;
+    const curr = queue.sort((c1, c2) => c2.r - c1.r).pop();
+    curr.visited = true;
+    
+    if(curr.x === end.x && curr.y === end.y) return calculatePath(curr);
     
     const adjs = [];
-    if(curr.x - 1 >= 0) adjs.push({ x: curr.x - 1, y: curr.y, risk: Number.MAX_VALUE, prev: null, visited: false });
-    if(curr.x + 1 < input[0].length) adjs.push({ x: curr.x + 1, y: curr.y, risk: Number.MAX_VALUE, prev: null });
-    if(curr.y - 1 >= 0) adjs.push({ x: curr.x, y: curr.y - 1, risk: Number.MAX_VALUE, prev: null });
-    if(curr.y + 1 < input.length) adjs.push({ x: curr.x, y: curr.y + 1, risk: Number.MAX_VALUE, prev: null });
+    if(curr.x - 1 >= 0) adjs.push(input[curr.y][curr.x - 1]);
+    if(curr.x + 1 < input[0].length) adjs.push(input[curr.y][curr.x + 1]);
+    if(curr.y - 1 >= 0) adjs.push(input[curr.y - 1][curr.x]);
+    if(curr.y + 1 < input.length) adjs.push(input[curr.y + 1][curr.x]);
 
     adjs.forEach(adj => {
-      const { x, y, risk } = adj;
+      const { risk, r, visited } = adj;
 
-      const newRisk = curr.risk + input[y][x];
-      if(newRisk < risk) {
-        adj.risk = newRisk;
-        adj.prev = [curr.y, curr.x];
-        queue.push(adj);
+      if(!visited) {
+        const newRisk = risk + curr.r;
+
+        if(newRisk < r) {
+          adj.r = newRisk;
+          adj.prev = [curr.y, curr.x];
+          queue.push(adj);
+        }
       }
     });
   }
--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ import day12 from './day-12/index.js';
 import day13 from './day-13/index.js';
 import day14 from './day-14/index.js';
 import day15 from './day-15/index.js';
+import day16 from './day-16/index.js';
 // <NEW_DAY_IMPORT>
 import generateDay from './helpers/generateDay.js';
 
@@ -40,6 +41,7 @@ const main = async () => {
 		case '13': console.log(PART === '1' ? await day13.part1() : await day13.part2()); break;
 		case '14': console.log(PART === '1' ? await day14.part1() : await day14.part2()); break;
 		case '15': console.log(PART === '1' ? await day15.part1() : await day15.part2()); break;
+		case '16': console.log(PART === '1' ? await day16.part1() : await day16.part2()); break;
 		// <NEW_DAY_CASE>
     case 'generateDay': generateDay(PART); break;
   }
