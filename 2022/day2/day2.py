c809aaaa1ab2dc6b84e5b1b9437120f4b6411ea9
River Marks <rmarks6767@gmail.com>
2022-12-02 12:24:18 -0500

    Day 2

--- 2022/day2/day2.py
+++ 2022/day2/day2.py
@@ -4,10 +4,83 @@ def get_parse():
   txt = get_input(2)
   return txt.split('\n')
 
-def day_2():
-  txt = get_parse()
-  
-  # Code Goes here
+outcomes = {
+  "A": { 
+    "type": "ROCK", 
+    "beats": "SCISSORS", 
+    "beatenBy": "PAPER", 
+    "score": 1 
+  },
+  "X": { 
+    "method": "LOSE", 
+    "type": "ROCK", 
+    "beats": "SCISSORS", 
+    "beatenBy": "PAPER", 
+    "score": 1 
+  },
+  "B": { 
+    "type": "PAPER", 
+    "beats": "ROCK", 
+    "beatenBy": "SCISSORS", 
+    "score": 2 
+  },
+  "Y": { 
+    "method": "DRAW", 
+    "type": "PAPER", 
+    "beats": "ROCK", 
+    "beatenBy": "SCISSORS", 
+    "score": 2 
+  },
+  "C": { 
+    "type": "SCISSORS", 
+    "beats": "PAPER", 
+    "beatenBy": "ROCK", 
+    "score": 3 
+  },
+  "Z": { 
+    "method": "WIN", 
+    "type": "SCISSORS", 
+    "beats": "PAPER", 
+    "beatenBy": "ROCK", 
+    "score": 3 
+  },
+}
 
-  print(f'Part 1 answer: ')
-  print(f'Part 2 answer: ')
+scores = {
+  "ROCK": 1,
+  "PAPER": 2,
+  "SCISSORS": 3
+}
+
+def day_2():
+  plays = get_parse()
+
+  ans1 = 0
+  ans2 = 0
+
+  for play in plays:
+    if play == '':
+      continue
+
+    [p1, p2] = play.split(' ')
+
+    o1 = outcomes.get(p1)
+    o2 = outcomes.get(p2)
+    
+    ans1 += o2.get('score')
+
+    if o2.get('method') == 'LOSE':
+      ans2 += scores.get(o1.get('beats'))
+    if o2.get('method') == 'DRAW':
+      ans2 += scores.get(o1.get('type')) + 3
+    if o2.get('method') == 'WIN':
+      ans2 += scores.get(o1.get('beatenBy')) + 6
+
+    if (o1.get('type') == o2.get('type')):
+      ans1 += 3
+    else:
+      if o2.get('beats') == o1.get('type'):
+        ans1 += 6
+
+  print(f'Part 1 answer: {ans1}')
+  print(f'Part 2 answer: {ans2}')
